bda6a2450294e2306d18bd8819bf8d799ff0cd41
VeronicaDiGiac <veronicadigiacomo190@gmail.com>
2025-07-03 15:57:41 +0200

    aggiustato env.private

--- src/environments/environments.prod.ts
+++
@@ -1,8 +0,0 @@
-declare var process: { env: { [key: string]: string } };
-
-export const environment = {
-  production: true,
-  emailJsServiceId: process.env['NG_APP_EMAILJS_SERVICE_ID'] || '',
-  emailJsTemplateId: process.env['NG_APP_EMAILJS_TEMPLATE_ID'] || '',
-  emailJsPublicKey: process.env['NG_APP_EMAILJS_PUBLIC_KEY'] || '',
-};
